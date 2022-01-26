0011cc9a70bd1ec19f565565a88630a1e63a9e19
= <hotalog@hota1024.com>
2022-01-26 20:02:21 +0900

    :lipstick: add setup page

---
+++ src/pages/setup.tsx
@@ -0,0 +1,15 @@
+import { Setup } from '@/page-components/Setup/Setup'
+import { NextPage } from 'next'
+
+/**
+ * Setup page.
+ */
+export const SetupPage: NextPage = () => {
+  return (
+    <>
+      <Setup />
+    </>
+  )
+}
+
+export default SetupPage
