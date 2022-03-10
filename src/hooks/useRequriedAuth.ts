0cb09e448f1226e6cca555805ce5773ddd0ca46e
= <hotalog@hota1024.com>
2022-03-11 00:59:18 +0900

    ✨add useRequiredAuth hook

---
+++ src/hooks/useRequriedAuth.ts
@@ -0,0 +1,45 @@
+import { useAuthChecking, useCurrentUser } from '@/atoms/auth'
+import { UserRole } from '@/graphql/generated'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+/**
+ *
+ * @param role role.
+ */
+export const useRequiredAuth = (role: UserRole) => {
+  const [authChecking] = useAuthChecking()
+  const [currentUser] = useCurrentUser()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (authChecking) {
+      // checking...
+      return
+    }
+
+    if (currentUser) {
+      const { role: userRole } = currentUser
+      const isAdmin = userRole === UserRole.Admin
+      const isNormal = isAdmin || userRole === UserRole.Normal
+
+      if (role === UserRole.Admin && isAdmin) {
+        // ok
+        return
+      }
+
+      if (role === UserRole.Normal && isNormal) {
+        // ok
+        return
+      }
+    }
+
+    router.push('/login', {
+      query: {
+        to: router.asPath,
+      },
+    })
+  }, [currentUser, authChecking, router, role])
+
+  return authChecking
+}
