1d8fb44af80834ea0f1cab7f6ac004adddce35b0
= <hotalog@hota1024.com>
2022-03-29 22:08:09 +0900

    💄update FullscreenLoading component

--- src/components/FullscreenLoading.tsx
+++ src/components/FullscreenLoading.tsx
@@ -1,14 +1,23 @@
-import { Box, Container, LinearProgress } from '@mui/material'
+import { Box, Container, LinearProgress, Typography } from '@mui/material'
+
+const types = {
+  default: '読込中です...',
+  authenticating: '認証中です...',
+} as const
 
 /**
  * FullscreenLoading props.
  */
-export type FullscreenLoadingProps = {}
+export type FullscreenLoadingProps = {
+  type?: keyof typeof types
+}
 
 /**
  * FullscreenLoading component.
  */
 export const FullscreenLoading: React.VFC<FullscreenLoadingProps> = (props) => {
+  const message = types[props.type ?? 'default']
+
   return (
     <Box
       height="100vh"
@@ -17,6 +26,9 @@ export const FullscreenLoading: React.VFC<FullscreenLoadingProps> = (props) => {
       alignItems="center"
     >
       <Container maxWidth="xs">
+        <Typography variant="h6" textAlign="center">
+          {message}
+        </Typography>
         <LinearProgress />
       </Container>
     </Box>
