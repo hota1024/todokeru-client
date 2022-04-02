bc04efad775755cb2584c083a9aa060055917f3a
= <hotalog@hota1024.com>
2022-04-03 04:43:19 +0900

    💄add previous button

--- src/page-components/AdminHeader.tsx
+++ src/page-components/AdminHeader.tsx
@@ -1,20 +1,29 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
+import Link from 'next/link'
 
 /**
  * AdminHeader props.
  */
 export type AdminHeaderProps = {
   title: string
+  previousText?: string
+  previousHref?: string
 }
 
 /**
  * AdminHeader component.
  */
 export const AdminHeader: React.VFC<AdminHeaderProps> = (props) => {
-  const { title } = props
+  const { title, previousText, previousHref } = props
+  const hasPrevious = !!previousText && !!previousHref
 
   return (
     <Box sx={{ mb: 2 }}>
+      {hasPrevious && (
+        <Link href={previousHref} passHref>
+          <Button color="inherit">{previousText}</Button>
+        </Link>
+      )}
       <Typography component="h2" variant="h4">
         {title}
       </Typography>
