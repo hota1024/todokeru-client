6b308b05096ad54dfedc0ab1c94104a7441d3ae1
= <hotalog@hota1024.com>
2022-04-03 20:21:53 +0900

    💄add error alerts

--- src/page-components/Register/Register.tsx
+++ src/page-components/Register/Register.tsx
@@ -1,7 +1,13 @@
 import { useCurrentUser } from '@/atoms/auth'
 import { FullscreenLoading } from '@/components/FullscreenLoading'
+import {
+  useIsRegisterationReceptableQuery,
+  useValidateRegisterationTokenMutation,
+} from '@/graphql/generated'
 import { Alert, Button, Container } from '@mui/material'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
 /**
  * Register props.
@@ -13,8 +19,28 @@ export type RegisterProps = {}
  */
 export const Register: React.VFC<RegisterProps> = (props) => {
   const [{ currentUser, isValidating }] = useCurrentUser()
+  const router = useRouter()
+  const { data: receptable, loading: loadingReceptable } =
+    useIsRegisterationReceptableQuery()
+  const [validatingToken, setValidatingToken] = useState(false)
+  const [validateSuccess, setValidateSuccess] = useState(false)
+  const [validateToken] = useValidateRegisterationTokenMutation()
 
-  if (isValidating) {
+  useEffect(() => {
+    if (typeof router.query.token === 'string') {
+      setValidatingToken(true)
+      validateToken({
+        variables: {
+          token: router.query.token,
+        },
+      }).then((data) => {
+        setValidateSuccess(data.data?.validateRegisterationToken ?? false)
+        setValidatingToken(false)
+      })
+    }
+  }, [router.query.token, validateToken])
+
+  if (isValidating || loadingReceptable || validatingToken) {
     return <FullscreenLoading />
   }
 
@@ -29,7 +55,41 @@ export const Register: React.VFC<RegisterProps> = (props) => {
             </Link>
           }
         >
-          既にログインしています。
+          既にログインしています。情報の更新は「ユーザーメニュー」で行うことが出来ます。
+        </Alert>
+      </Container>
+    )
+  }
+
+  if (receptable?.isRegisterationReceptable === false) {
+    return (
+      <Container maxWidth="sm" sx={{ pt: 4 }}>
+        <Alert
+          severity="warning"
+          action={
+            <Link href="/login" passHref>
+              <Button>ログイン画面へ</Button>
+            </Link>
+          }
+        >
+          現在、登録を受け付けていません。
+        </Alert>
+      </Container>
+    )
+  }
+
+  if (!validateSuccess) {
+    return (
+      <Container maxWidth="sm" sx={{ pt: 4 }}>
+        <Alert
+          severity="error"
+          action={
+            <Link href="/login" passHref>
+              <Button>ログイン画面へ</Button>
+            </Link>
+          }
+        >
+          登録に必要な資格情報を確認することが出来ませんでした。
         </Alert>
       </Container>
     )
