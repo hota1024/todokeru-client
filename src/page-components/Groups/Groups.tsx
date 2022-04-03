6eafd293ca9317431fd9e304319e710ba48deb07
= <hotalog@hota1024.com>
2022-04-03 19:07:38 +0900

    💄add groups page

---
+++ src/page-components/Groups/Groups.tsx
@@ -0,0 +1,58 @@
+import { useGroupsQuery } from '@/graphql/generated'
+import { AdminLayout } from '@/layouts/AdminLayout/AdminLayout'
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  Divider,
+  LinearProgress,
+  List,
+  ListItem,
+  ListItemText,
+  ListSubheader,
+} from '@mui/material'
+import Link from 'next/link'
+import { AdminHeader } from '../AdminHeader'
+
+/**
+ * Groups props.
+ */
+export type GroupsProps = {}
+
+/**
+ * Groups component.
+ */
+export const Groups: React.VFC<GroupsProps> = (props) => {
+  const { data: groupsData, loading: loadingData } = useGroupsQuery()
+
+  const groups = groupsData?.groups ?? []
+
+  return (
+    <AdminLayout>
+      <AdminHeader title="クラスの管理" />
+      <Card variant="outlined">
+        {loadingData && <LinearProgress />}
+        <List subheader={<ListSubheader>クラス一覧</ListSubheader>}>
+          {groups.map((group) => (
+            <Link href={`/admin/groups/${group.id}`} passHref key={group.id}>
+              <ListItem component="a" button>
+                <ListItemText
+                  primary={group.name}
+                  secondary={`${group.students.length}人が所属中`}
+                />
+              </ListItem>
+            </Link>
+          ))}
+        </List>
+        <Divider />
+        <CardActions>
+          <Box flexGrow={1} />
+          <Link href="/admin/groups/new" passHref>
+            <Button component="a">新しいクラスを追加</Button>
+          </Link>
+        </CardActions>
+      </Card>
+    </AdminLayout>
+  )
+}
---
+++ src/pages/admin/groups/index.tsx
@@ -0,0 +1,11 @@
+import { Groups } from '@/page-components/Groups/Groups'
+import { NextPage } from 'next'
+
+/**
+ * Groups page.
+ */
+export const GroupsPage: NextPage = () => {
+  return <Groups />
+}
+
+export default GroupsPage
