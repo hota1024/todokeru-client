6136b0398abbcc1a76f040570a6c6ed00f37c36f
= <hotalog@hota1024.com>
2022-05-08 20:14:22 +0900

    🔨add static page generation timeout

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  staticPageGenerationTimeout: 600,
   webpack(config, options) {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src')
 
